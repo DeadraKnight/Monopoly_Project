eeecfe3522848973cd52baddbb347bad67fc9fbc
thelukestor <51249854+thelukestor@users.noreply.github.com>
2024-04-10 21:54:26 -0400

    Rules
    Created a rules on scene 1. (Need to fix where you can only open and close the rules once)

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public class ExitButton : MonoBehaviour
+{
+    public GameObject Panel; // Reference to the panel that displays the rules
+
+    public void OnExitButtonClick()
+    {
+        Panel.SetActive(false); // Set the visibility of the rules panel to false
+    }
+
+  
+}
