48973facc52813e73819896c7764cd857b731c37
DeadraKnight <93338145+DeadraKnight@users.noreply.github.com>
2024-04-24 22:20:09 -0400

    Cleaned up Pawn Move If statement

--- Assets/Scripts/Pawn Components/Pawn.cs
+++ Assets/Scripts/Pawn Components/Pawn.cs
@@ -59,6 +59,10 @@ public sealed class Pawn : NetworkBehaviour
         GameManager.Instance.EndTurn();
     }
 
+    /// <summary>
+    /// Method to move the pawn on the board
+    /// </summary>
+    /// <param name="steps"></param>
     [ServerRpc(RequireOwnership = false)]
     public void Move(int steps)
     {
@@ -87,20 +91,12 @@ public sealed class Pawn : NetworkBehaviour
             // Check if the new position is the "Go To Jail" tile
             if (currentPosition == 30)
             {
-                // Move the pawn to the "Jail" waypoint
-                transform.position = jailWaypoint.transform.position;
-
-                // Set currentWP to 10 to fix the pathing.
-                currentPosition = 10;
-
-                // Set the isInJail flag to true
-                controllingPlayer.isInJail = true;
+                GoToJail();
             }
             //if statement to check if the player has looped around the board
             if (currentPosition < steps)
             {
-                // Add 200 to the players balance
-                controllingPlayer.Balance += 200;
+                PassGo();
             }
             // Check if current tile is a ChanceTile
             if (Board.Instance.Tiles[currentPosition].ChanceTile)
@@ -110,42 +106,90 @@ public sealed class Pawn : NetworkBehaviour
             }
             if (Board.Instance.Tiles[currentPosition].owned == true)
             {
-                if (Board.Instance.Tiles[currentPosition].owningPlayer != controllingPlayer)
-                {
-                    int rent = Board.Instance.Tiles[currentPosition].rent; // Get the rent amount of the tile
-
-                    controllingPlayer.Balance -= rent; // Deduct rent from player balance
-
-                    Board.Instance.Tiles[currentPosition].owningPlayer.Balance += rent; // Add rent to the owning player's balance
-
-                    Debug.Log($"Player {controllingPlayer.OwnerId} paid ${rent} in rent to Player {Board.Instance.Tiles[currentPosition].owningPlayer.OwnerId}!");
-                }
-                else
-                {
-                    // Do nothing
-                }
-                
+                PayRent();    
             }
             if (Board.Instance.Tiles[currentPosition].TaxTile)
             {
-                int taxAmount = Board.Instance.Tiles[currentPosition].TaxCost;
-
-                // Deduct tax from player balance
-                controllingPlayer.Balance -= taxAmount;
-
-                // Add tax to the tax pile
-                Board.Instance.TaxPile += taxAmount;
-
-                // (Optional) Inform the player about the tax payment
-                Debug.Log($"Player {controllingPlayer.username} paid ${taxAmount} in tax!");
+                LandOnTaxTile();
             }
             if (currentPosition == 20)
             {
-                controllingPlayer.Balance += Board.Instance.TaxPile;
-                Board.Instance.TaxPile = 0;
+                LandOnFreeParking();
             }
         });
 
         tween.Play();
     }
+    #region Board Actions
+
+    /// <summary>
+    /// Method to send the player to the "Jail" waypoint
+    /// </summary>
+    public void GoToJail()
+    {
+        // Move the pawn to the "Jail" waypoint
+        transform.position = jailWaypoint.transform.position;
+
+        // Set currentWP to 10 to fix the pathing.
+        currentPosition = 10;
+
+        // Set the isInJail flag to true
+        controllingPlayer.isInJail = true;
+    }
+
+    /// <summary>
+    /// Method to pass "Go" and collect $200
+    /// </summary>
+    public void PassGo()
+    {
+        controllingPlayer.Balance += 200;
+    }
+
+    /// <summary>
+    /// Method to pay rent to the owning player
+    /// </summary>
+    public void PayRent()
+    {
+        if (Board.Instance.Tiles[currentPosition].owningPlayer != controllingPlayer)
+        {
+            int rent = Board.Instance.Tiles[currentPosition].rent; // Get the rent amount of the tile
+
+            controllingPlayer.Balance -= rent; // Deduct rent from player balance
+
+            Board.Instance.Tiles[currentPosition].owningPlayer.Balance += rent; // Add rent to the owning player's balance
+
+            Debug.Log($"Player {controllingPlayer.OwnerId} paid ${rent} in rent to Player {Board.Instance.Tiles[currentPosition].owningPlayer.OwnerId}!");
+        }
+        else
+        {
+            // Do nothing
+        }
+    }
+
+    /// <summary>
+    /// Method to pay tax when landing on a tax tile
+    /// </summary>
+    public void LandOnTaxTile()
+    {
+        int taxAmount = Board.Instance.Tiles[currentPosition].TaxCost;
+
+        // Deduct tax from player balance
+        controllingPlayer.Balance -= taxAmount;
+
+        // Add tax to the tax pile
+        Board.Instance.TaxPile += taxAmount;
+
+        // (Optional) Inform the player about the tax payment
+        Debug.Log($"Player {controllingPlayer.username} paid ${taxAmount} in tax!");
+    }
+
+    /// <summary>
+    /// Method to collect the money from the tax pile when landing on Free Parking
+    /// </summary>
+    public void LandOnFreeParking()
+    {
+        controllingPlayer.Balance += Board.Instance.TaxPile;
+        Board.Instance.TaxPile = 0;
+    }
+    #endregion
 }
